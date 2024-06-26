e5e568ca49a9d8e168e05bbf9ca00312ac71d427
mdaffriza <164282858+mdaffriza@users.noreply.github.com>
2024-06-26 11:44:19 +0700

    Update Player.java

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -16,38 +16,48 @@ public class Player extends Entity {
     GamePanel gamePanel;
     KeyHandler keyHandler;
 
+
     private int spriteCounter = 0;
     private int spriteNum = 1;
     private boolean restrictMovement = true;  // New field to control movement restriction
 
     private BufferedImage up1, up2, up3, down1, down2, down3;
     private BufferedImage left1, left2, left3, right1, right2, right3;
+    private BufferedImage hpImage, speedImage;
     public final int screenX;
     public final int screenY;
     private Set<Integer> visitableTiles;
     private int hp;
+    private int speed;
 
     public Player(GamePanel gamePanel, KeyHandler keyHandler) {
         this.gamePanel = gamePanel;
         this.keyHandler = keyHandler;
         screenX = gamePanel.screenWidth / 2 - (gamePanel.tileSize / 2);
         screenY = gamePanel.screenHeight / 2 - (gamePanel.tileSize / 2);
-        hp = 100;
+
+        hp = 2;    // Set default HP to 5
+        speed = 5; // Set default speed to 5
 
         setDefaultValues();
         getPlayerImage();
         initializeVisitableTiles();
+        setRestrictMovement(true);
     }
     // Method to increase HP
     public void increaseHp(int amount) {
-        hp += amount;
-//        System.out.println("HP increased! Current HP: " + hp);
+        int maxHp = 5; // Nilai maksimum HP
+        while (amount > 0 && hp < maxHp) {
+            hp += 1;
+            amount -= 1;
+        }
     }
+
 
     public void setDefaultValues() {
         worldX = gamePanel.tileSize * 23; // 23
         worldY = gamePanel.tileSize * 91; // 91
-        speed = 4;
+        speed = 5;
         direction = "down";
     }
 
@@ -65,6 +75,8 @@ public class Player extends Entity {
             right1 = loadImage("/res/player/tile008.png");
             right2 = loadImage("/res/player/tile009.png");
             right3 = loadImage("/res/player/tile010.png");
+            hpImage = loadImage("/res/player/hp.png"); // Gambar untuk HP
+            speedImage = loadImage("/res/player/speed.png"); // Gambar untuk kecepatan
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1); // Exit if images are not found
@@ -80,22 +92,24 @@ public class Player extends Entity {
     }
 
     private void initializeVisitableTiles() {
-        visitableTiles = Set.of(4703, 4702, 2233, 2188, 2417, 2416, 2418, 2373, 2372, 2371, 2321, 2276, 2240, 3088, 2141,
-                2325, 2322, 2277, 2096, 2143, 2323, 2144, 2324, 2280, 2236, 2326, 2328, 2234, 2189, 4747, 4792,
-                4834, 4879, 4701, 4700, 4699, 4698, 4697, 4696, 4695, 4694, 4745, 4744, 4743, 4742, 4741, 4740,
-                4746, 4748, 4749, 4750, 4751, 4706, 4661, 4660, 4036, 2442, 2401, 2356, 2311, 2307, 2308, 2309,
-                2310, 2402, 2403, 2358, 2357, 2443, 2352, 2397, 2315, 2314, 2329, 2269, 2150, 2445, 2444, 2319,
-                2320, 2398, 2353, 2051, 2494, 2495, 2184, 2139, 2237, 2327, 4431, 4432, 2316, 2185, 2140, 2238,
-                4433, 2369, 2230, 2229, 2274, 2275, 4427, 4428, 4429, 4430, 2331, 2376, 2421, 3311, 3312, 2183,
-                2228, 4382, 4338, 4339, 4340, 4341, 4342, 4387, 4386, 4383, 4384, 4385, 2399, 2354, 2577, 2532,
-                2487, 2425, 2380, 2335, 2147, 2948, 2330, 2151, 2488, 3251, 3252, 3253, 3209, 3206, 2337, 2336,
-                2486, 2273, 2489, 2135, 2136, 2137, 2332, 2405, 2406, 2460, 4208, 4207, 4206, 4205, 4204, 4203,
-                4202, 2426, 2381, 4073, 4072, 4071, 4070, 4069, 4068, 4067, 2377, 4023, 2415, 4022, 2370, 4024,
-                4114, 4113, 4112, 2422, 4115, 4116, 4117, 4027, 4025, 4026, 4118, 2427, 2400, 2411, 2312, 2404,
-                2412, 3749, 3750, 3751, 3752, 3753, 3754, 3799, 3798, 3800, 3801, 3802, 3803, 3804, 3805, 3806,
-                3761, 3716, 3488, 3487, 3486, 3485, 3484, 3483, 3482, 2587, 2542, 3443, 3398, 3851, 3852, 2284,
-                2193, 2192, 2194, 3343, 3342, 3297, 2148, 2949, 2903, 2902, 2901, 2900, 2899, 2898, 2897, 2896,
-                2895, 2904, 2905, 2906, 2907, 2908, 2909, 2910, 2911, 2946, 3298 );
+        visitableTiles = Set.of(
+                2051, 2096, 2135, 2136, 2137, 2139, 2140, 2141, 2143, 2144,
+                2147, 2148, 2150, 2151, 2183, 2184, 2188, 2189, 2192,
+                2193, 2194, 2228, 2229, 2230, 2233, 2234, 2236, 2237, 2238, 2269, 2273,
+                2274, 2275, 2276, 2277, 2280, 2284, 2307, 2308, 2309, 2310, 2311, 2312, 2314,
+                2315, 2316, 2319, 2320, 2321, 2322, 2323, 2324, 2325, 2326,
+                2327, 2328, 2329, 2330, 2331, 2332, 2335, 2336, 2337, 2352, 2356, 2369,
+                2372, 2376, 2380, 2381, 2397, 2398, 2399,
+                2401, 2402, 2403, 2405, 2406,
+                2417, 2421, 2422, 2425, 2426, 2427, 2442, 2443,
+                2444, 2445, 2460, 2486, 2487, 2488, 2489, 2494, 2495, 2532,
+                2542, 2577, 2587, 2900, 2901, 2902, 2903, 2904, 2905, 2906,
+                2907, 2908, 2909, 2910, 2911, 2946, 2948, 2949, 3206,
+                3209, 3251, 3252, 3253, 3297, 3298, 3311, 3312, 3342, 3343,
+                3398, 3443, 3482, 3483, 3484, 3485, 3486, 3487, 3488, 3716,
+                3749, 3750, 3751, 3752, 3753, 3754, 3761, 3798, 3799, 3800,
+                3801, 3802, 3803, 3804, 3805, 3806, 3851, 3852
+        );
     }
 
     public void update() {
@@ -159,6 +173,18 @@ public class Player extends Entity {
         int tileCode = gamePanel.getTileManager().mapTileNum[tileX][tileY];
         return gamePanel.getTileManager().isTileVisitable(tileCode);
     }
+    // Method to decrease HP
+    public void decreaseHp(int amount) {
+        hp -= amount;
+        if (hp < 0) hp = 0;
+        // Add logic to handle player death if needed
+    }
+
+    // Method to decrease speed
+    public void decreaseSpeed(int amount) {
+        speed -= amount;
+        if (speed < 1) speed = 1; // Minimum speed
+    }
 
     public void draw(Graphics2D g2) {
         BufferedImage image = null;
@@ -209,6 +235,26 @@ public class Player extends Entity {
         int newHeight = (int) (gamePanel.tileSize / aspectRatio);
 
         g2.drawImage(image, screenX, screenY, newWidth, newHeight, null);
+        // Draw HP and Speed representations
+        drawStatus(g2);
+    }
+
+    private void drawStatus(Graphics2D g2) {
+        int smallerTileSize = gamePanel.tileSize / 2; // Ukuran gambar yang lebih kecil dua kali lipat
+
+        // Draw HP representation
+        int hpX = 10;
+        int hpY = 10;
+        for (int i = 0; i < hp; i++) {
+            g2.drawImage(hpImage, hpX + (i * smallerTileSize), hpY, smallerTileSize, smallerTileSize, null);
+        }
+
+        // Draw Speed representation
+        int speedX = 10;
+        int speedY = 10 + smallerTileSize + 10;
+        for (int i = 0; i < speed; i++) {
+            g2.drawImage(speedImage, speedX + (i * smallerTileSize), speedY, smallerTileSize, smallerTileSize, null);
+        }
     }
 
     public void setRestrictMovement(boolean restrictMovement) {  // New method to enable/disable movement restriction
